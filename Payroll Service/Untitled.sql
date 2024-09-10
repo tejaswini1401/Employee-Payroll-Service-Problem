f469e60fd7094586e460572bb99feaead9e0d0ed
tejaswini_14 <shelaketejaswini14@gmail.com>
2024-09-10 12:23:53 +0530

    Added: CREATE TABLE IN DB

--- Payroll Service/Untitled.sql
+++ Payroll Service/Untitled.sql
@@ -2,3 +2,10 @@ CREATE DATABASE payroll_service;
 show databases;
 USE payroll_service;
 SELECT DATABASE();
+CREATE TABLE employee_payroll(
+	id INT unsigned NOT NULL AUTO_INCREMENT,
+    name VARCHAR(150) NOT NULL,
+    salary Double NOT NULL,
+    start Date NOT NULL,
+    PRIMARY KEY (id)
+);
